744340cd374bcf72ceb42ee00a48f90753d5012d
Qhite <hwlim2000@naver.com>
2025-04-15 16:17:02 +0000

    ok

--- eval.py
+++ eval.py
@@ -5,7 +5,7 @@ import net
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = net.DepthNet(lidar_point=60, bin_size=256).to(device)
-model.load_state_dict(torch.load("RUN_04-15-10-03-56.pth.tar"))
+model.load_state_dict(torch.load("RUN_04-15-10-03-56.pth.tar", weights_only=True))
 model.eval()
 
 import dataloader
