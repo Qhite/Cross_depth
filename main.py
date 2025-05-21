9544049a673a50883c067dc44a4cafa01a71654d
Qhite <hwlim2000@naver.com>
2025-05-21 12:01:03 +0000

    ok

--- main.py
+++ main.py
@@ -21,3 +21,18 @@ class encap(nn.Module):
 test = encap()
 flops, macs, params = get_model_profile(model=test, input_shape=(1,1), print_profile=False)
 print(f"FLOPs: {flops/1000000000:.2f}G | MACS: {macs/1000000000:.2f}G | Params: {params/1000000:.2f}M")
+print("\n\n\n")
+
+# ========================================================================================================================
+
+from torch.profiler import profile, record_function, ProfilerActivity
+
+model = net.DepthNet(lidar_point=60, f_size=[320,8,10], feat_ch=[320, 112, 40, 24, 16], d_model=256, bin_size=64).cuda()
+
+dummy_x = torch.rand([B, 3, 228, 304]).cuda()
+dummy_y = torch.rand([B, 1, 60]).cuda()
+
+with profile(activities=[ProfilerActivity.CUDA], record_shapes=True) as prof:
+    with record_function("model_inference"):
+        model(dummy_x, dummy_y)
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
